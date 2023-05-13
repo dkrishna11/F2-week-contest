4d4b61ecb2f4d8bf494ca18365eed0ce72f479fc
Krishna_D <dkrishna051997@gmail.com>
2023-05-13 21:12:29 +0530

    f2-contest

--- script.js
+++ script.js
@@ -8,20 +8,47 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+    if( student.marks>50 ){
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(student){
+    if(student.marks>50 ){
+      console.log(student)
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4, name:"susan", age:"20" , marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+ let failedStudents = arr.filter(function(student){
+  if(student.marks<50)
+  return false;
+  else return true;
+ });
+ console.log(failedStudents);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "krishna", age: "26", marks: 90 },
+    { id: 6, name: "Vishruth", age: "20", marks: 60 },
+    { id: 7, name: "Nandini", age: "24", marks: 80 },
+  ];
+
+  let concatArray = arr.concat(newArr);
+  console.log(concatArray);
 }
